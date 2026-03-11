9882dc6aae4a3a8649b4cd174d4e6a753f6c030d
TatheerB <60303339@udst.edu.qa>
2026-03-11 19:48:10 +0300

    Test with Docker Hub token

--- app.py
+++ app.py
@@ -100,3 +100,4 @@ if __name__ == '__main__':
     db.init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+"# Testing with Docker Hub token" 
